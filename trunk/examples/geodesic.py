f6bff5c170c9afdbb5649dbea3797c230127db68
whitaker.jeffrey <whitaker.jeffrey@3b505dfa-a830-0410-85e6-77c0f0d768dc>
2011-02-04 18:17:18 +0000

    color oceans

--- trunk/examples/geodesic.py
+++ trunk/examples/geodesic.py
@@ -1,19 +1,20 @@
 from mpl_toolkits.basemap import Basemap
 from spharm import getgeodesicpts
-import pylab as p
+import matplotlib.pyplot as plt
+import numpy as np
 # set up orthographic map projection.
-map = Basemap(projection='ortho',lat_0=50,lon_0=-100,resolution='l')
+map = Basemap(projection='ortho',lat_0=45,lon_0=-100,resolution='l')
 # draw coastlines, country boundaries, fill continents.
 map.drawcoastlines()
-map.fillcontinents(color='coral')
+map.fillcontinents(color='coral',lake_color='aqua')
 # draw the edge of the map projection region (the projection limb)
-map.drawmapboundary()
+map.drawmapboundary(fill_color='aqua')
 # draw lat/lon grid lines every 30 degrees.
-map.drawmeridians(p.arange(0,360,30))
-map.drawparallels(p.arange(-90,90,30))
+map.drawmeridians(np.arange(0,360,30))
+map.drawparallels(np.arange(-90,90,30))
 m = int(raw_input('input the number of points on the edge of each spherical geodesic triangle:'))
 # find the geodesic points.
 lats, lons = getgeodesicpts(m)
 x, y = map(lons, lats)
 map.scatter(x,y,marker='o',c='b',s=20,zorder=10)
-p.show()
+plt.show()
