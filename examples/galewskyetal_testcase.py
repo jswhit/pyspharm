e70ee5d057535f6a0660d36b0b966b3a05d4b4a2
whitaker.jeffrey <whitaker.jeffrey@3b505dfa-a830-0410-85e6-77c0f0d768dc>
2012-02-13 02:19:33 +0000

    more cleanups.

--- examples/galewskyetal_testcase.py
+++ examples/galewskyetal_testcase.py
@@ -1,5 +1,5 @@
 import numpy as np
-from spharm import Spharmt, getspecindx, gaussian_lats_wts
+from spharm import Spharmt, getspecindx
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap, addcyclic
 import time
@@ -10,59 +10,49 @@ import time
 # shallow-water equations"
 # http://www-vortex.mcs.st-and.ac.uk/~rks/reprints/galewsky_etal_tellus_2004.pdf
 
-# grid size
-nlon = 256 
-nlat = 128
-# spectral truncation
-ntrunc = 85  
-gridtype = 'gaussian'
-legfunc = 'stored'
-rsphere = 6.37122e6
+# grid, time step info
+nlons = 256  # number of longitudes
+ntrunc = nlons/3 # spectral truncation (for alias-free computations)
+nlats = (nlons/2)+1 # for regular grid.
+gridtype = 'regular'
 dt = 150 # time step in seconds
 itmax = 6*(86400/dt) # integration length in days
 
-pi = np.pi
-d2r = pi/180.
+pi = np.pi; d2r = pi/180.
 # parameters for test
-omega = 7.292e-5
-grav = 9.80616
-hbar = 10.e3
-umax = 80.
-phi0 = pi/7.
-phi1 = 0.5*pi - phi0
-phi2 = 0.25*pi
+rsphere = 6.37122e6 # earth radius
+omega = 7.292e-5 # rotation rate
+grav = 9.80616 # gravity
+hbar = 10.e3 # resting depth
+umax = 80. # jet speed
+phi0 = pi/7.; phi1 = 0.5*pi - phi0; phi2 = 0.25*pi
 en = np.exp(-4.0/(phi1-phi0)**2)
-alpha = 1./3.
-beta = 1./15.
-hamp = 120.
+alpha = 1./3.; beta = 1./15.
+hamp = 120. # amplitude of height perturbation to zonal jet
 efold = 3.*3600. # efolding timescale at ntrunc for hyperdiffusion
 ndiss = 8 # order for hyperdiffusion
 
 # setup up spherical harmonic instance, set lats/lons of grid
 
-x = Spharmt(nlon,nlat,rsphere,gridtype=gridtype,legfunc=legfunc)
-gaulats,weights = gaussian_lats_wts(nlat)
-gaulats = d2r*gaulats
-gaulons = np.arange(-pi,pi,2.*pi/nlon)
-lons,lats = np.meshgrid(gaulons,gaulats)
+x = Spharmt(nlons,nlats,rsphere,gridtype=gridtype)
+delta = 2.*pi/nlons
+lats1d = 0.5*pi-delta*np.arange(nlats)
+lons1d = np.arange(-pi,pi,delta)
+lons,lats = np.meshgrid(lons1d,lats1d)
 f = 2.*omega*np.sin(lats) # coriolis
 
 # zonal jet.
-vg = np.zeros((nlat,nlon),np.float32)
-u1 = (umax/en)*np.exp(1./((gaulats-phi0)*(gaulats-phi1)))
-ug = np.zeros((nlat),np.float32)
-ug = np.where(np.logical_and(gaulats < phi1, gaulats > phi0), u1, ug)
-ug.shape = (nlat,1)
-ug = ug*np.ones((nlat,nlon),dtype=np.float32)
+vg = np.zeros((nlats,nlons),np.float32)
+u1 = (umax/en)*np.exp(1./((lats1d-phi0)*(lats1d-phi1)))
+ug = np.zeros((nlats),np.float32)
+ug = np.where(np.logical_and(lats1d < phi1, lats1d > phi0), u1, ug)
+ug.shape = (nlats,1)
+ug = ug*np.ones((nlats,nlons),dtype=np.float32) # broadcast to shape (nlats,nlonss)
+# height perturbation.
 hbump = hamp*np.cos(lats)*np.exp(-(lons/alpha)**2)*np.exp(-(phi2-lats)**2/beta)
 
 # initial vorticity, divergence in spectral space
 vrtspec, divspec =  x.getvrtdivspec(ug,vg,ntrunc)
-
-# create spectral tendency arrays
-ddivdtspec = np.zeros(vrtspec.shape+(3,), np.complex64)
-dvrtdtspec = np.zeros(vrtspec.shape+(3,), np.complex64)
-dphidtspec = np.zeros(vrtspec.shape+(3,), np.complex64)
 
 # create spectral indexing arrays, laplacian operator and its inverse.
 indxm, indxn = getspecindx(ntrunc)
@@ -81,13 +71,15 @@ phispec = ilap*tmpspec1 - tmpspec2
 phig = grav*(hbar + hbump) + x.spectogrd(phispec)
 phispec = x.grdtospec(phig,ntrunc)
 
+# initialize spectral tendency arrays
+ddivdtspec = np.zeros(vrtspec.shape+(3,), np.complex64)
+dvrtdtspec = np.zeros(vrtspec.shape+(3,), np.complex64)
+dphidtspec = np.zeros(vrtspec.shape+(3,), np.complex64)
+nnew = 0; nnow = 1; nold = 2
+
 # time step loop.
 
 time1 = time.clock()
-
-nnew = 0
-nnow = 1
-nold = 2
 for ncycle in range(itmax+1):
     t = ncycle*dt
 # get vort,u,v,phi on grid
@@ -141,8 +133,8 @@ print 'CPU time = ',time2-time1
 
 # make a NH Lambert aziumthal plot.
 m = Basemap(projection='nplaea',boundinglat=1,lon_0=270,round=True)
-vrtg,gaulons2 = addcyclic(vrtg,gaulons/d2r)
-lons, lats = np.meshgrid(gaulons2,gaulats/d2r)
+vrtg,lons1d = addcyclic(vrtg,lons1d/d2r)
+lons, lats = np.meshgrid(lons1d,lats1d/d2r)
 x,y = m(lons,lats)
 levs = np.arange(-1.5e-4,1.501e-4,1.5e-5)
 m.drawmeridians(np.arange(-180,181,60))
