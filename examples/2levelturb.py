5958e6f04acd99d341689bb97e08fcceae336eb9
whitaker.jeffrey <whitaker.jeffrey@3b505dfa-a830-0410-85e6-77c0f0d768dc>
2012-02-17 23:34:38 +0000

    use matplotlib.animation module

--- examples/2levelturb.py
+++ examples/2levelturb.py
@@ -2,20 +2,19 @@ import numpy as np
 from spharm import Spharmt
 from twolevel import TwoLevel
 import numpy.random as npran
-import gobject, sys, time
+import matplotlib.animation as animation
 
 # animates solution from twolevel.py on screen, using matplotlib.
-# requires GTKAgg matplotlib backend.
 
 # set model parameters.
-nlons = 256  # number of longitudes
+nlons = 128  # number of longitudes
 ntrunc = nlons/3 # spectral truncation (for alias-free computations)
 nlats = (nlons/2)+1 # for regular grid.
 gridtype = 'regular'
-dt = 300 # time step in seconds
+dt = 900 # time step in seconds
 tdiab = 12.*86400 # thermal relaxation time scale
 tdrag = 4.*86400. # lower layer drag
-efold = 1.*3600. # hyperdiffusion time scale
+efold = 4*dt # hyperdiffusion time scale
 rsphere = 6.37122e6 # earth radius
 jetexp = 2
 umax = 40
@@ -33,8 +32,8 @@ vg = np.zeros((sp.nlat,sp.nlon,2),np.float32)
 ug = model.uref
 vrtspec, divspec = sp.getvrtdivspec(ug,vg,model.ntrunc)
 psispec = np.zeros(vrtspec.shape, vrtspec.dtype)
-psispec.real += npran.normal(scale=1.e3,size=(psispec.shape))
-psispec.imag += npran.normal(scale=1.e3,size=(psispec.shape))
+psispec.real += npran.normal(scale=1.e4,size=(psispec.shape))
+psispec.imag += npran.normal(scale=1.e4,size=(psispec.shape))
 vrtspec = vrtspec + model.lap[:,np.newaxis]*psispec
 thetaspec = model.nlbalance(vrtspec)
 divspec = np.zeros(thetaspec.shape, thetaspec.dtype)
@@ -44,7 +43,6 @@ model.heat = np.zeros(model.theta.shape, model.theta.dtype)
 
 # animate vorticity from solution as it is running
 import matplotlib
-matplotlib.use('GTKAgg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap, addcyclic
 fig = plt.figure(figsize=(8,10))
@@ -58,7 +56,7 @@ data2,lons1dx = addcyclic(model.theta,lons1d)
 lons, lats = np.meshgrid(lons1dx,lats1d)
 x,y = m(lons,lats)
 m.drawmeridians(np.arange(-180,180,60),labels=[0,0,0,1],ax=ax1)
-m.drawparallels(np.arange(-60,91,30),labels=[1,0,0,0],ax=ax2)
+m.drawparallels(np.arange(-60,91,30),labels=[1,0,0,0],ax=ax1)
 m.drawmeridians(np.arange(-180,180,60),labels=[0,0,0,1],ax=ax2)
 m.drawparallels(np.arange(-60,91,30),labels=[1,0,0,0],ax=ax2)
 levs1 = np.arange(-1.6e-4,1.61e-4,2.e-5)
@@ -71,11 +69,10 @@ t = 0.
 txt1 = ax1.set_title('Upper Level Vorticity (T%s, hour %6.2f)' % (ntrunc,t/3600.))
 txt2 = ax2.set_title('Temperature (T%s, hour %6.2f)' % (ntrunc,t/3600.))
 
-manager = plt.get_current_fig_manager()
-# callback function to update contour plot
+
 def updatefig(*args):
-    global cnt,vrtspec,divspec,thetaspec,CS1,CS2
-    t = cnt*model.dt
+    global vrtspec,divspec,thetaspec,CS1,CS2,txt1,txt2,t
+    t += model.dt
     vrtspec, divspec, thetaspec = model.rk4step(vrtspec, divspec, thetaspec)
     data1,lons1dx = addcyclic(model.vrt[:,:,1],lons1d)
     data2,lons1dx = addcyclic(model.theta,lons1d)
@@ -86,12 +83,6 @@ def updatefig(*args):
     CS2=m.contourf(x,y,data2,levs2,cmap=plt.cm.spectral,extend='both',ax=ax2)
     txt1.set_text('Upper Level Vorticity (T%s, hour %6.2f)' % (ntrunc,t/3600.))
     txt2.set_text('Temperature (T%s, hour %6.2f)' % (ntrunc,t/3600.))
-    manager.canvas.draw()
-    cnt = cnt+1
-    return True
 
-cnt = 0 # image counter
-gobject.idle_add(updatefig)
-sys.stdout.write('close window to exit ...\n')
+ani = animation.FuncAnimation(fig, updatefig)
 plt.show()
-sys.stdout.write('done\n')
